84524bddb3d8dc9d830c03479c70167bb7cca2d2
JuppLCD <lorenzocanovas207@gmail.com>
2023-03-03 22:06:42 -0300

    First steps: generating the matrix to later fill with the schedules

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -2,89 +2,23 @@ import { useState } from 'react';
 
 import type { ChangeEvent } from 'react';
 
-enum Days {
-	MONDAY = 'Monday',
-	TUESDAY = 'Tuesday',
-	WEDNESDAY = 'Wednesday',
-	THURSDAY = 'Thursday',
-	FRIDAY = 'Friday',
-	SATURDAY = 'Saturday',
-	SUNDAY = 'Sunday',
-}
+import { subjectsExample } from './../data';
+import GenerateSchedules from '../utils/GenerateSchedules';
 
 function Layout() {
 	const [time, setTime] = useState('08:30');
+
+	const data = [...subjectsExample];
 
 	const handleChangeInputTime = (e: ChangeEvent<HTMLInputElement>) => {
 		setTime(e.target.value);
 	};
 
-	// Ejempolo basico sobre una pestaña con sus respectivas materias y horarios de cursado
-	const dataExample = {
-		title: 'Horarios de la facultad',
-		subjects: [
-			{
-				id: 1,
-				subject: 'Mathematics',
-				possible_schedules: [
-					{
-						id: 1,
-						name: 'Comicion 1',
-						time: {
-							day: Days.THURSDAY,
-							start: '08:30',
-							end: '10:00',
-						},
-					},
-					{
-						id: 2,
-						name: 'Comicion 2',
-						time: {
-							day: Days.TUESDAY,
-							start: '13:30',
-							end: '15:00',
-						},
-					},
-					{
-						id: 3,
-						name: 'Comicion 3',
-						time: {
-							day: Days.WEDNESDAY,
-							start: '13:30',
-							end: '15:00',
-						},
-					},
-				],
-			},
-			{
-				id: 2,
-				subject: 'Literature',
-				possible_schedules: [
-					{
-						id: 1,
-						name: 'Comicion 1',
-						time: {
-							day: Days.MONDAY,
-							start: '08:30',
-							end: '10:00',
-						},
-					},
-					{
-						id: 2,
-						name: 'Comicion 2',
-						time: {
-							day: Days.MONDAY,
-							start: '13:30',
-							end: '15:00',
-						},
-					},
-				],
-			},
-		],
+	const createSchedules = () => {
+		const horarios = new GenerateSchedules(data);
+
+		console.log('La matiz que servira de base para crear todos los posibles horarios es => ', horarios.scheduleMatrix);
 	};
-	/*
-	La idea es crear una matriz en la cual las filas sean "los dias" y las columnas "los posibles horarios", y una vez que el usuario coloque todos los datos sobre las materias y sus posibles horarios, recorrer cada uno para crear todos los posibles 
-    */
 	return (
 		<>
 			<header>
@@ -96,21 +30,30 @@ function Layout() {
 				</label>
 
 				<br />
-				<h2 className='text-2xl font-bold'>{dataExample.title}</h2>
-				<ul>
-					{dataExample.subjects.map((subject) => (
-						<li key={subject.id} className='mb-5'>
-							<h3 className='text-2xl'>{subject.subject}</h3>
-							<ul>
-								{subject.possible_schedules.map((schedule) => (
-									<li key={schedule.id}>
-										{schedule.name} - {schedule.time.day} - {schedule.time.start} to {schedule.time.end}
-									</li>
-								))}
-							</ul>
-						</li>
-					))}
-				</ul>
+				{data && (
+					<>
+						<h2 className='text-3xl font-bold'>Tus materias y sus posibles horarios:</h2>
+						<ul>
+							{data.map((subject) => (
+								<li key={subject.id} className='mb-5'>
+									<h3 className='text-2xl'>{subject.subject}</h3>
+									<ul>
+										{subject.possible_schedules.map((schedule) => (
+											<li key={schedule.id}>
+												{schedule.name} - {schedule.time.day} - {schedule.time.start} to {schedule.time.end}
+											</li>
+										))}
+									</ul>
+								</li>
+							))}
+						</ul>
+					</>
+				)}
+				<div className='mt-5'>
+					<button type='button' onClick={createSchedules}>
+						Generar horarios
+					</button>
+				</div>
 			</main>
 			<footer></footer>
 		</>
