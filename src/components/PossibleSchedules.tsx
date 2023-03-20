4e0b202021c1131019e3f086f16639a46fc15dfc
JuppLCD <lorenzocanovas207@gmail.com>
2023-03-19 23:26:10 -0300

    Modifying PossibleSchedules and Schedule components to paint the matter and its commission with color

--- src/components/PossibleSchedules.tsx
+++ src/components/PossibleSchedules.tsx
@@ -18,12 +18,27 @@ function PossibleSchedules({ data }: Props) {
 	}
 	const hoursArr = data.getTimeColumnInStringFormat();
 
+	const generateRandomColor = () => {
+		const r = Math.floor(Math.random() * 256);
+		const g = Math.floor(Math.random() * 256);
+		const b = Math.floor(Math.random() * 256);
+
+		const rgbColor = `rgb(${r},${g},${b})`;
+
+		return rgbColor;
+	};
+
+	const subjects = data.subjects.map((subject) => {
+		subject.rgbColor = generateRandomColor();
+		return subject;
+	});
+
 	return (
 		<div>
 			<h1>
 				Posibles Horarios
 				{allPossibleSchedules.map((possibleSchedule, i) => (
-					<Schedule data={possibleSchedule} key={i} hoursArr={hoursArr} />
+					<Schedule data={possibleSchedule} key={i} hoursArr={hoursArr} subjects={subjects} />
 				))}
 			</h1>
 		</div>
--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -1,11 +1,29 @@
+import { ISubject } from '../types/Subject';
 import { IPossibleSchedule } from '../utils/GenerateSchedules';
 
 interface PropsSchedule {
 	data: IPossibleSchedule;
 	hoursArr: string[];
+	subjects: ISubject[];
 }
-function Schedule({ data, hoursArr }: PropsSchedule) {
+function Schedule({ data, hoursArr, subjects }: PropsSchedule) {
 	const dia = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+	const coloringSubject = (e: number | string) => {
+		if (typeof e == 'number') return '';
+
+		const subject = subjects.find((subject) => subject.id === parseInt(e.split('-')[0]));
+		return subject?.rgbColor as string;
+	};
+
+	const title = (e: string) => {
+		const ids = e.split('-');
+		const subject = subjects.find((subject) => subject.id === parseInt(ids[0]));
+
+		const schedule = subject?.possible_schedules.find((schedule) => schedule.id === parseInt(ids[1]));
+
+		return schedule?.name + ' - ' + subject?.subject ?? subject?.subject ?? '';
+	};
 
 	return (
 		<>
@@ -19,8 +37,12 @@ function Schedule({ data, hoursArr }: PropsSchedule) {
 
 							<ul>
 								{day?.map((e, index) => (
-									<li key={index} className='text-center p-1'>
-										{e == -1 ? <br /> : e}
+									<li
+										key={index}
+										className={`text-center p-1 h-8 ${e === -1 ? '' : 'overflow-y-scroll'}`}
+										style={{ background: coloringSubject(e) }}
+									>
+										{e == -1 ? <br /> : title(e as string)}
 									</li>
 								))}
 							</ul>
@@ -38,18 +60,20 @@ interface PropsScheduleBase {
 }
 function ScheduleBase({ children, hoursArr }: PropsScheduleBase) {
 	return (
-		<section className='flex border-2 mx-2' style={{ width: 900 }}>
-			{/* Horarios */}
-			<ul className='w-28 pt-12 border-x-2'>
-				{hoursArr.map((hora) => (
-					<li key={hora} className='text-center p-1 border-t-2'>
-						{hora}
-					</li>
-				))}
-			</ul>
+		<section className='overflow-x-auto'>
+			<div className='flex border-2 mx-2' style={{ width: 900 }}>
+				{/* Horarios */}
+				<ul className='w-28 pt-12 border-x-2'>
+					{hoursArr.map((hora) => (
+						<li key={hora} className='text-center p-1 border-t-2'>
+							{hora}
+						</li>
+					))}
+				</ul>
 
-			{/* Dias */}
-			<div className='flex-1'>{children}</div>
+				{/* Dias */}
+				<div className='flex-1'>{children}</div>
+			</div>
 		</section>
 	);
 }
