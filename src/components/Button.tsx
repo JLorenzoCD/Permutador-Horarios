3b7ffaf6ed1ec343d1bcfae2ed40b8e1398c0dc6
Lorenzo.Canovas <jlorenzocanovas@gmail.com>
2023-04-03 22:13:02 -0300

    refactor: :art: Passing the component logic to the src/utils/components folder

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,41 +1,13 @@
 import type { ButtonHTMLAttributes } from 'react';
 
+import { themeButton } from '../utils/components/Button';
+
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
 	theme?: 'default' | 'outline';
 	color?: 'blue' | 'red' | 'purple' | 'yellow' | 'green' | 'light' | 'dark';
 }
 function Button({ className, theme = 'default', color = 'blue', ...props }: Props) {
 	let buttonStyles = 'font-bold py-2 px-4 rounded mr-2 mb-2';
-
-	const themeButton = {
-		default: {
-			blue: 'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300',
-			red: 'focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300',
-			purple: 'focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300',
-			yellow: 'focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300',
-			green: 'focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300',
-
-			light:
-				'text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200',
-			dark: 'text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300',
-		},
-		outline: {
-			styleBase: 'bg-transparent border hover:border-transparent',
-
-			blue: 'text-blue-700 hover:text-white border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300',
-			red: 'text-red-700 hover:text-white border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300',
-			purple:
-				'text-purple-700 hover:text-white border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300',
-			yellow:
-				'text-yellow-400 hover:text-white border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300',
-			green:
-				'text-green-700 hover:text-white border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300',
-
-			light:
-				'text-white hover:text-gray-900 border-white hover:bg-white focus:ring-4 focus:outline-none focus:ring-gray-200',
-			dark: 'text-gray-900 hover:text-white border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300',
-		},
-	};
 
 	if (theme === 'outline') {
 		buttonStyles += ` ${className} ${themeButton.outline.styleBase} ${themeButton.outline[color]}`;
