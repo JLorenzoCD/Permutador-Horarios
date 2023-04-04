15ed74be61241c107d951dc1b8e23e7743775959
Lorenzo.Canovas <jlorenzocanovas@gmail.com>
2023-04-03 22:12:37 -0300

    refactor: :art: Passing the component logic to the src/utils/components folder

--- src/components/FormSubjects.tsx
+++ src/components/FormSubjects.tsx
@@ -1,12 +1,9 @@
-import { useState } from 'react';
-
-import { defaultSchedule, defaultSubject } from '../data';
 import { Days } from '../utils/Days';
-import generateRandomColor from '../utils/generateRandomColor';
 
 import Button from './Button';
 
-import type { ChangeEvent, FormEvent } from 'react';
+import FromSubjectsUtils from '../utils/components/FromSubjects';
+
 import type { ISchedule, ISubject } from '../types/Subject';
 
 interface Props {
@@ -15,87 +12,12 @@ interface Props {
 	addSchedule: (subjectId: number, newSchedule: ISchedule) => void;
 }
 function FormSubjects({ subjects, addSubject, addSchedule }: Props) {
-	const [subject, setSubject] = useState({ ...defaultSubject, hexColor: generateRandomColor() });
-	const [schedule, setSchedule] = useState({ ...defaultSchedule, subjectId: subjects[0].id ?? 0 });
-
-	const handleChangeSubject = (e: ChangeEvent<HTMLInputElement>) => {
-		setSubject((prevState) => {
-			return { ...prevState, [e.target.name]: e.target.value };
-		});
-	};
-
-	const handleChangeSchedule = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-		if (e.target.name === 'name' || e.target.name === 'subjectId') {
-			setSchedule((prevState) => {
-				return { ...prevState, [e.target.name]: e.target.value };
-			});
-		} else {
-			setSchedule((prevState) => {
-				return { ...prevState, time: { ...prevState.time, [e.target.name]: e.target.value } };
-			});
-		}
-	};
-
-	const handleSubmitSubject = (e: FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-
-		if (subject.subject.trim() === '') {
-			alert('No es posible una materia sin nombre');
-			return;
-		}
-		const nombreOcupado = subjects.some(
-			(subjectSave) => subjectSave.subject.trim().toUpperCase() === subject.subject.trim().toUpperCase()
-		);
-		if (nombreOcupado) {
-			alert('No es posible repetir el mismo nombre para una materia');
-			return;
-		}
-		const newSubject = {
-			id: new Date().getTime(),
-			subject: subject.subject,
-			possible_schedules: [],
-			hexColor: subject.hexColor,
-		};
-
-		addSubject(newSubject);
-		setSubject({ ...defaultSubject, hexColor: generateRandomColor() });
-	};
-
-	const handleSubmitSchedule = (e: FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-
-		if (schedule.name.trim() === '') {
-			alert('No es posible una materia sin nombre');
-			return;
-		}
-
-		const subjectSave = subjects.find((subject) => subject.id === Number(schedule.subjectId));
-
-		if (!subjectSave) {
-			alert('Error al buscar en materias');
-			return;
-		}
-
-		const nombreOcupado = subjectSave.possible_schedules.some(
-			(scheduleSave) => scheduleSave.name.trim().toUpperCase() === schedule.name.trim().toUpperCase()
-		);
-		if (nombreOcupado) {
-			alert('No es posible repetir el mismo nombre para un orario en la misma materia');
-			return;
-		}
-
-		const newSchedule = {
-			id: new Date().getTime(),
-			name: schedule.name,
-			time: { ...schedule.time }, // TODO: Tengo que revisar que el horairo de finalizacion no sea menor a el horarios de comienzo
-		};
-
-		addSchedule(Number(schedule.subjectId), newSchedule);
-	};
+	const { subject, schedule, handleChangeSubject, handleChangeSchedule, createSubject, createSchedule } =
+		FromSubjectsUtils(subjects);
 
 	return (
 		<div>
-			<form onSubmit={handleSubmitSubject}>
+			<form onSubmit={createSubject(addSubject)}>
 				<h2 className='text-2xl'>Create new Subject</h2>
 				<label className='block'>
 					Subject:
@@ -121,7 +43,7 @@ function FormSubjects({ subjects, addSubject, addSchedule }: Props) {
 			</form>
 			<br />
 
-			<form onSubmit={handleSubmitSchedule}>
+			<form onSubmit={createSchedule(addSchedule)}>
 				<h2 className='text-2xl'>Create new Schedule</h2>
 
 				<label className='block'>
---
+++ src/utils/components/FromSubjects.ts
@@ -0,0 +1,95 @@
+import { useState } from 'react';
+
+import { defaultSchedule, defaultSubject } from '../../data';
+import generateRandomColor from '../generateRandomColor';
+
+import type { ChangeEvent, FormEvent } from 'react';
+import { ISchedule, ISubject } from '../../types/Subject';
+
+function FromSubjects(subjects: ISubject[]) {
+	const [subject, setSubject] = useState({ ...defaultSubject, hexColor: generateRandomColor() });
+	const [schedule, setSchedule] = useState({ ...defaultSchedule, subjectId: subjects[0].id ?? 0 });
+
+	const handleChangeSubject = (e: ChangeEvent<HTMLInputElement>) => {
+		setSubject((prevState) => {
+			return { ...prevState, [e.target.name]: e.target.value };
+		});
+	};
+
+	const handleChangeSchedule = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+		if (e.target.name === 'name' || e.target.name === 'subjectId') {
+			setSchedule((prevState) => {
+				return { ...prevState, [e.target.name]: e.target.value };
+			});
+		} else {
+			setSchedule((prevState) => {
+				return { ...prevState, time: { ...prevState.time, [e.target.name]: e.target.value } };
+			});
+		}
+	};
+
+	const createSubject = (addSubject: (newSubject: ISubject) => void) => {
+		return (e: FormEvent<HTMLFormElement>) => {
+			e.preventDefault();
+
+			if (subject.subject.trim() === '') {
+				alert('No es posible una materia sin nombre');
+				return;
+			}
+			const nombreOcupado = subjects.some(
+				(subjectSave) => subjectSave.subject.trim().toUpperCase() === subject.subject.trim().toUpperCase()
+			);
+			if (nombreOcupado) {
+				alert('No es posible repetir el mismo nombre para una materia');
+				return;
+			}
+			const newSubject = {
+				id: new Date().getTime(),
+				subject: subject.subject,
+				possible_schedules: [],
+				hexColor: subject.hexColor,
+			};
+
+			addSubject(newSubject);
+			setSubject({ ...defaultSubject, hexColor: generateRandomColor() });
+		};
+	};
+
+	const createSchedule = (addSchedule: (subjectId: number, newSchedule: ISchedule) => void) => {
+		return (e: FormEvent<HTMLFormElement>) => {
+			e.preventDefault();
+
+			if (schedule.name.trim() === '') {
+				alert('No es posible una materia sin nombre');
+				return;
+			}
+
+			const subjectSave = subjects.find((subject) => subject.id === Number(schedule.subjectId));
+
+			if (!subjectSave) {
+				alert('Error al buscar en materias');
+				return;
+			}
+
+			const nombreOcupado = subjectSave.possible_schedules.some(
+				(scheduleSave) => scheduleSave.name.trim().toUpperCase() === schedule.name.trim().toUpperCase()
+			);
+			if (nombreOcupado) {
+				alert('No es posible repetir el mismo nombre para un orario en la misma materia');
+				return;
+			}
+
+			const newSchedule = {
+				id: new Date().getTime(),
+				name: schedule.name,
+				time: { ...schedule.time }, // TODO: Tengo que revisar que el horairo de finalizacion no sea menor a el horarios de comienzo
+			};
+
+			addSchedule(Number(schedule.subjectId), newSchedule);
+		};
+	};
+
+	return { subject, schedule, handleChangeSubject, handleChangeSchedule, createSubject, createSchedule };
+}
+
+export default FromSubjects;
