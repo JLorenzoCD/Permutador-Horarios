9496e8d7266225a810c91ca3a365e0c04113042d
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-04-22 19:09:22 -0300

    feat: :white_check_mark: subjectsExample is modified

--- src/data/index.ts
+++ src/data/index.ts
@@ -6,53 +6,125 @@ import { Days } from '../utils/Days';
 export const subjectsExample: ISubject[] = [
 	{
 		id: 1,
-		subject: 'Mathematics',
+		subject: 'Discrete Mathematics I',
 		possible_schedules: [
 			{
-				id: 1,
-				name: 'Comicion 1',
-				time: [
-					{
-						day: Days.THURSDAY,
-						start: '08:30',
-						end: '10:00',
-						id: 1,
-					},
-					{
-						day: Days.SATURDAY,
-						start: '08:30',
-						end: '10:00',
-						id: 2,
-					},
-				],
-			},
-			{
-				id: 2,
-				name: 'Comicion 2',
+				id: 7,
+				name: 'Com. 1',
 				time: [
 					{
 						day: Days.TUESDAY,
-						start: '13:30',
-						end: '15:00',
-						id: 3,
+						start: '09:00',
+						end: '11:00',
+						id: 13,
 					},
 					{
-						day: Days.MONDAY,
-						start: '13:30',
-						end: '15:00',
-						id: 4,
+						day: Days.TUESDAY,
+						start: '11:00',
+						end: '13:00',
+						id: 14,
+					},
+					{
+						day: Days.THURSDAY,
+						start: '09:00',
+						end: '11:00',
+						id: 15,
+					},
+					{
+						day: Days.THURSDAY,
+						start: '11:00',
+						end: '13:00',
+						id: 16,
 					},
 				],
 			},
 			{
-				id: 3,
-				name: 'Comicion 3',
+				id: 8,
+				name: 'Com. 2',
 				time: [
 					{
-						day: Days.WEDNESDAY,
-						start: '13:30',
-						end: '15:00',
-						id: 5,
+						day: Days.TUESDAY,
+						start: '09:00',
+						end: '11:00',
+						id: 17,
+					},
+					{
+						day: Days.TUESDAY,
+						start: '11:00',
+						end: '13:00',
+						id: 18,
+					},
+					{
+						day: Days.THURSDAY,
+						start: '09:00',
+						end: '11:00',
+						id: 19,
+					},
+					{
+						day: Days.THURSDAY,
+						start: '11:00',
+						end: '13:00',
+						id: 20,
+					},
+				],
+			},
+			{
+				id: 9,
+				name: 'Com. 3',
+				time: [
+					{
+						day: Days.TUESDAY,
+						start: '14:00',
+						end: '16:00',
+						id: 21,
+					},
+					{
+						day: Days.TUESDAY,
+						start: '16:00',
+						end: '18:00',
+						id: 22,
+					},
+					{
+						day: Days.THURSDAY,
+						start: '14:00',
+						end: '16:00',
+						id: 50,
+					},
+					{
+						day: Days.THURSDAY,
+						start: '16:00',
+						end: '18:00',
+						id: 51,
+					},
+				],
+			},
+			{
+				id: 10,
+				name: 'Com. 4',
+				time: [
+					{
+						day: Days.TUESDAY,
+						start: '14:00',
+						end: '16:00',
+						id: 23,
+					},
+					{
+						day: Days.TUESDAY,
+						start: '16:00',
+						end: '18:00',
+						id: 24,
+					},
+					{
+						day: Days.THURSDAY,
+						start: '14:00',
+						end: '16:00',
+						id: 55,
+					},
+					{
+						day: Days.THURSDAY,
+						start: '16:00',
+						end: '18:00',
+						id: 56,
 					},
 				],
 			},
@@ -60,35 +132,113 @@ export const subjectsExample: ISubject[] = [
 	},
 	{
 		id: 2,
-		subject: 'Literature',
+		subject: 'Introduction to Algorithms I',
 		possible_schedules: [
 			{
-				id: 4,
-				name: 'Comicion 1',
+				id: 1,
+				name: 'Com. 1',
 				time: [
 					{
 						day: Days.MONDAY,
-						start: '08:30',
-						end: '10:00',
-						id: 6,
+						start: '09:00',
+						end: '13:00',
+						id: 1,
 					},
 					{
-						day: Days.FRIDAY,
-						start: '10:30',
-						end: '12:00',
+						day: Days.WEDNESDAY,
+						start: '09:00',
+						end: '13:00',
+						id: 2,
+					},
+				],
+			},
+			{
+				id: 2,
+				name: 'Com. 2',
+				time: [
+					{
+						day: Days.MONDAY,
+						start: '09:00',
+						end: '13:00',
+						id: 3,
+					},
+					{
+						day: Days.WEDNESDAY,
+						start: '09:00',
+						end: '13:00',
+						id: 4,
+					},
+				],
+			},
+			{
+				id: 4,
+				name: 'Com. 3',
+				time: [
+					{
+						day: Days.MONDAY,
+						start: '14:00',
+						end: '18:00',
 						id: 7,
+					},
+					{
+						day: Days.WEDNESDAY,
+						start: '14:00',
+						end: '18:00',
+						id: 8,
 					},
 				],
 			},
 			{
 				id: 5,
-				name: 'Comicion 2',
+				name: 'Com. 4',
 				time: [
 					{
 						day: Days.MONDAY,
-						start: '13:30',
-						end: '15:00',
-						id: 8,
+						start: '14:00',
+						end: '18:00',
+						id: 9,
+					},
+					{
+						day: Days.WEDNESDAY,
+						start: '14:00',
+						end: '18:00',
+						id: 10,
+					},
+				],
+			},
+			{
+				id: 3,
+				name: 'Com. 5 (Virtual)',
+				time: [
+					{
+						day: Days.MONDAY,
+						start: '09:00',
+						end: '13:00',
+						id: 5,
+					},
+					{
+						day: Days.WEDNESDAY,
+						start: '09:00',
+						end: '13:00',
+						id: 6,
+					},
+				],
+			},
+			{
+				id: 6,
+				name: 'Com. 6 (Virtual)',
+				time: [
+					{
+						day: Days.MONDAY,
+						start: '15:00',
+						end: '19:00',
+						id: 11,
+					},
+					{
+						day: Days.WEDNESDAY,
+						start: '15:00',
+						end: '19:00',
+						id: 12,
 					},
 				],
 			},
@@ -96,35 +246,113 @@ export const subjectsExample: ISubject[] = [
 	},
 	{
 		id: 3,
-		subject: 'English',
+		subject: 'Mathematical Analysis I',
 		possible_schedules: [
 			{
-				id: 6,
-				name: 'Comicion 1',
+				id: 11,
+				name: 'Com. 1',
 				time: [
 					{
-						day: Days.MONDAY,
-						start: '10:30',
-						end: '12:00',
-						id: 9,
+						day: Days.WEDNESDAY,
+						start: '09:00',
+						end: '13:00',
+						id: 30,
 					},
 					{
-						day: Days.WEDNESDAY,
-						start: '08:30',
-						end: '10:00',
-						id: 10,
+						day: Days.FRIDAY,
+						start: '09:00',
+						end: '13:00',
+						id: 31,
 					},
 				],
 			},
 			{
-				id: 7,
-				name: 'Comicion 2',
+				id: 12,
+				name: 'Com. 2',
 				time: [
 					{
-						day: Days.TUESDAY,
-						start: '11:00',
-						end: '13:30',
-						id: 11,
+						day: Days.WEDNESDAY,
+						start: '09:00',
+						end: '13:00',
+						id: 32,
+					},
+					{
+						day: Days.FRIDAY,
+						start: '09:00',
+						end: '13:00',
+						id: 33,
+					},
+				],
+			},
+			{
+				id: 15,
+				name: 'Com. 3',
+				time: [
+					{
+						day: Days.WEDNESDAY,
+						start: '14:00',
+						end: '18:00',
+						id: 38,
+					},
+					{
+						day: Days.FRIDAY,
+						start: '14:00',
+						end: '18:00',
+						id: 39,
+					},
+				],
+			},
+			{
+				id: 16,
+				name: 'Com. 4',
+				time: [
+					{
+						day: Days.WEDNESDAY,
+						start: '14:00',
+						end: '18:00',
+						id: 40,
+					},
+					{
+						day: Days.FRIDAY,
+						start: '14:00',
+						end: '18:00',
+						id: 41,
+					},
+				],
+			},
+			{
+				id: 13,
+				name: 'Com. 5 (Virtual)',
+				time: [
+					{
+						day: Days.WEDNESDAY,
+						start: '09:00',
+						end: '13:00',
+						id: 34,
+					},
+					{
+						day: Days.FRIDAY,
+						start: '09:00',
+						end: '13:00',
+						id: 35,
+					},
+				],
+			},
+			{
+				id: 14,
+				name: 'Com. 6 (Virtual)',
+				time: [
+					{
+						day: Days.WEDNESDAY,
+						start: '14:00',
+						end: '18:00',
+						id: 36,
+					},
+					{
+						day: Days.FRIDAY,
+						start: '14:00',
+						end: '18:00',
+						id: 37,
 					},
 				],
 			},
